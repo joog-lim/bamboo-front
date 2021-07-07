1f5fc0a277f67c9c0cedef7834d26122ab5a0558
iseolin76 <iseolin76@gmail.com>
2021-07-08 02:38:45 +0900

    Add: module scss 지원

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,3 +1,7 @@
+declare module "*.scss" {
+  export const content: { [className: string]: string };
+}
+
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
