7ce806c47d16af98dffec065a3fc267c8f2f1aa3
iseolin76 <iseolin76@gmail.com>
2021-07-12 23:59:12 +0900

    create: algorithms component

---
+++ src/components/algorithms/algorithms.tsx
@@ -0,0 +1,23 @@
+import style from "./style.module.scss";
+
+const Algorithms: React.FC = () => {
+  return (
+    <article className={style.algorithmsBox}>
+      <header>
+        <h3>#833번째 알고리즘</h3>
+        <p>2022년 5월 30일 새벽</p>
+      </header>
+      <h4>오늘은 뭐할까</h4>
+      <p>
+        오늘은 뭐할까. 무슨 이상한 이 피그마는 자꾸 내가 쓰는 글 못 알아먹기만
+        하고 난 디자이너가 아닌데 귀찮기만 하고, 하하하하하하오늘은 뭐할까. 무슨
+        이상한 이 피그마는 자꾸 내가 쓰는 글 못 알아먹기만 하고 난 디자이너가
+        아닌데 귀찮기만 하고, 하하하하하하 아니 내가 왜 이걸 하고 있냐고아니
+        내가 왜 이걸 하고 있냐고아니 내가 왜 이걸 하고 있냐고아니 내가 왜 이걸
+        하고 있냐고아니 내가 왜 이걸 하고 있냐고아니 내가 왜 이걸 하고 있냐고
+      </p>
+    </article>
+  );
+};
+
+export default Algorithms;
