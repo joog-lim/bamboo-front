69052783900e1e8db256f7a474de6efec8b725dc
iseolin76 <iseolin76@gmail.com>
2021-07-24 17:15:02 +0900

    add: recoilRoot

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from "next/app";
 import "styles/global.scss";
+import { RecoilRoot } from "recoil";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <RecoilRoot>
+      <Component {...pageProps} />
+    </RecoilRoot>
+  );
 }
 
 export default MyApp;
