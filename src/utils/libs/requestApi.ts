29278d147b0769d1b7634a0717c92c16ebd147ce
iseolin76 <iseolin76@gmail.com>
2021-08-23 19:58:25 +0900

    update: 베이스 헤더 삭제

--- src/utils/libs/requestApi.ts
+++ src/utils/libs/requestApi.ts
@@ -1,4 +1,4 @@
-import { BASE_HEADER, BASE_URL } from "constants/config.json";
+import { BASE_URL } from "constants/config.json";
 import axios, { AxiosRequestConfig } from "axios";
 
 export interface hasToken {
@@ -7,13 +7,13 @@ export interface hasToken {
 
 const RequestApi = (p: AxiosRequestConfig) => {
   try {
-    let header = Object.assign(BASE_HEADER, p.headers);
+    // let header = Object.assign(BASE_HEADER, p.headers);
     const res = axios({
       method: p.method,
       baseURL: BASE_URL,
       url: p.url,
       data: p.data,
-      headers: header,
+      headers: p.headers,
     });
     return res;
   } catch (error) {
