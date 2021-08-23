ea08e598751eefec02820d10bf1efb314f397b58
iseolin76 <iseolin76@gmail.com>
2021-08-23 20:02:28 +0900

    update: apply getPost function

--- src/components/algorithms/algorithms.tsx
+++ src/components/algorithms/algorithms.tsx
@@ -1,19 +1,17 @@
 import style from "./style.module.scss";
 import Header from "./item/headerPresenter";
+import { AlgorithmsProps } from "./algorithmsContainer";
 
-const Algorithms: React.FC = () => {
+const Algorithms: React.FC<AlgorithmsProps> = (p: AlgorithmsProps) => {
   return (
     <article className={style.algorithmsBox}>
-      <Header state="" />
-      <h4>오늘은 뭐할까</h4>
-      <p>
-        오늘은 뭐할까. 무슨 이상한 이 피그마는 자꾸 내가 쓰는 글 못 알아먹기만
-        하고 난 디자이너가 아닌데 귀찮기만 하고, 하하하하하하오늘은 뭐할까. 무슨
-        이상한 이 피그마는 자꾸 내가 쓰는 글 못 알아먹기만 하고 난 디자이너가
-        아닌데 귀찮기만 하고, 하하하하하하 아니 내가 왜 이걸 하고 있냐고아니
-        내가 왜 이걸 하고 있냐고아니 내가 왜 이걸 하고 있냐고아니 내가 왜 이걸
-        하고 있냐고아니 내가 왜 이걸 하고 있냐고아니 내가 왜 이걸 하고 있냐고
-      </p>
+      <Header
+        status={p.data.status}
+        createdAt={p.data.createdAt}
+        number={p.data.number}
+      />
+      <h4>{p.data.title}</h4>
+      <p>{p.data.content}</p>
     </article>
   );
 };
---
+++ src/components/algorithms/algorithmsContainer.tsx
@@ -0,0 +1,5 @@
+import { AlgorithmApi } from "src/types/types";
+
+export interface AlgorithmsProps {
+  data: AlgorithmApi;
+}
