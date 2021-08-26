51eaa6664f31313162247d300234fecba7cffda5
iseolin76 <iseolin76@gmail.com>
2021-08-26 11:11:13 +0900

    create: connect login api

---
+++ src/components/modal/loginModal/loginContainer.ts
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+
+import { isAdminState } from "recoil/atom";
+import auth from "utils/api/auth";
+
+const useLogin = () => {
+  const [password, setPassword] = useState("");
+  const [isAdmin, setIsAdmin] = useRecoilState(isAdminState);
+
+  const tryLogin = async () => {
+    const res = await auth.login(password);
+    if (res) {
+      setIsAdmin(res.data.success);
+      if (isAdmin) {
+        window.localStorage.setItem("token", res.data.token);
+        alert("성공적으로 로그인되었습니다.");
+      }
+    } else {
+      alert("비밀번호가 틀렸습니다.");
+    }
+  };
+
+  return [setPassword, tryLogin];
+};
+
+export default useLogin;
