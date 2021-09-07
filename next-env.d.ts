d997fe95cf13c1a1d30102e9f208f459d1127918
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-09-07 17:06:12 +0900

    Divide file to declare scss

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,3 @@
-declare module '*.scss' {
-    const content: { [className: string]: string };
-    export default content;
-}
+/// <reference types="next" />
+/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
