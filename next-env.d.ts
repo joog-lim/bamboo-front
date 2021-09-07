ad95d9bd0cf1366bce2c7c795a03a91b1f78dcfc
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-09-07 16:54:23 +0900

    Fix declare logic by css.module

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,7 @@
-declare module '*.scss';
+declare module '*.scss' {
+    const content: { [className: string]: string };
+    export default content;
+}
 /// <reference types="next" />
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
