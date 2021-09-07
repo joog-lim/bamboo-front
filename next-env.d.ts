8e173827f4e114b5c56ea2087fdc51645ee7c9a1
이선우 <60869316+sunwoo0706@users.noreply.github.com>
2021-09-07 16:57:01 +0900

    Remove next strict logic

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,6 +2,3 @@ declare module '*.scss' {
     const content: { [className: string]: string };
     export default content;
 }
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="next/image-types/global" />
