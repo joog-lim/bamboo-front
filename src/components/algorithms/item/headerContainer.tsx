20641e599cdadb97f601c375b586244bc7893f5b
iseolin76 <iseolin76@gmail.com>
2021-09-11 18:04:54 +0900

    update: timestamp를 정해진 표기법으로 변환g

--- src/components/algorithms/item/headerContainer.tsx
+++ src/components/algorithms/item/headerContainer.tsx
@@ -5,3 +5,13 @@ export interface HeaderProps {
   number: number | string;
   tag?: string;
 }
+
+export const getDate = (timestamp: number): string => {
+  const date = new Date(timestamp);
+  let time: string;
+  if (date.getHours() < 7) time = "새벽";
+  else if (date.getHours() < 12) time = "오전";
+  else time = "오후";
+  console.log("dfas");
+  return `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDate()}일 ${time}`;
+};
