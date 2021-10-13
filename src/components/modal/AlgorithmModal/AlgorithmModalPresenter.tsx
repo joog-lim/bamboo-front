86853a5a6ee9ca366207c6fc7068f70b7b97326d
iseolin76 <iseolin76@gmail.com>
2021-10-13 11:29:04 +0900

    Update: content, title 판별하여 value 지정

--- src/components/modal/AlgorithmModal/AlgorithmModalPresenter.tsx
+++ src/components/modal/AlgorithmModal/AlgorithmModalPresenter.tsx
@@ -10,8 +10,12 @@ const AlgorithmModal: React.FC<algorithmModalProps> = (
   p: algorithmModalProps
 ) => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [content, setContent] = useState("");
-  const [title, setTitle] = useState("");
+
+  //여기 초기값은 의미없는 값입니다.
+  //input에서 입력을 하였는 지 판별하기 위해 넣었습니다.
+  //좋은 방법이 있다면 알려주세요.
+  const [content, setContent] = useState("default32rewfdas");
+  const [title, setTitle] = useState("default32rewfdas");
   const [reason, setReason] = useState("");
 
   const [openModal, closeModal] = modalController(setModalIsOpen);
@@ -55,6 +59,9 @@ const AlgorithmModal: React.FC<algorithmModalProps> = (
   };
 
   const onClick = () => {
+    if (content === "default32rewfdas") {
+      alert("내용을 입력하여주세요.");
+    }
     switch (p.children) {
       case "삭제": {
         deletePost();
@@ -93,21 +100,25 @@ const AlgorithmModal: React.FC<algorithmModalProps> = (
           <h1 className={p.isRed ? s.redH1 : s.greenH1}>{p.children}하기</h1>
           {p.isHeading ? (
             <input
+              type="text"
               className={s.password}
               defaultValue={`알고리즘을 ${p.children}합니다`}
               readOnly
             />
           ) : (
             <input
+              type="text"
               className={s.password}
               placeholder="제목을 입력하세요."
               autoFocus={true}
               required
+              value={title === "default32rewfdas" ? p.title : title}
               onChange={({ target: { value } }) => setTitle(value)}
             />
           )}
           {p.isReason && (
             <input
+              type="text"
               className={s.password}
               placeholder="사유를 입력하세요."
               autoFocus={true}
@@ -120,6 +131,7 @@ const AlgorithmModal: React.FC<algorithmModalProps> = (
             required
             onChange={({ target: { value } }) => setContent(value)}
             placeholder="내용을 입력하세요."
+            value={content === "default32rewfdas" ? p.content : content}
           />
           <button className={p.isRed ? s.redBtn : s.greenBtn} onClick={onClick}>
             {p.children}
