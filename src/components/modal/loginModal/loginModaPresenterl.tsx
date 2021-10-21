e92d908f071cc9df42feb4ce98d05368121b5b05
iseolin76 <iseolin76@gmail.com>
2021-10-21 09:44:19 +0900

    Update: publish google login

--- src/components/modal/loginModal/loginModaPresenterl.tsx
+++ src/components/modal/loginModal/loginModaPresenterl.tsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 import { useRecoilState } from "recoil";
+import { GoogleLogin } from "react-google-login";
 
 import { isAdminState } from "recoil/atom";
 import s from "./loginModal.module.scss";
-import useLogin, { customStyles } from "./loginContainer";
+import useLogin, { customStyles, adminText, userText } from "./loginContainer";
 import modalController from "../modal";
 import SpinnerBar from "components/spinner/spinnerPresenter";
 import { loadingState } from "recoil/atom";
 
 const LoginModal: React.FC = () => {
+  const [isAdminLogin, setIsAdminLogin] = useState<boolean>(false);
   const [isAdmin, setIsAdmin] = useRecoilState(isAdminState);
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -34,9 +36,17 @@ const LoginModal: React.FC = () => {
     tryLogin("");
   };
 
+  const responseGoogle = (response: any) => {
+    console.log(response);
+  };
+
+  const onChangeState = () => {
+    setIsAdminLogin(!isAdminLogin);
+  };
+
   return (
     <>
-      <button onClick={onClick}>{isAdmin ? "사용자" : "관리자"}</button>
+      <button onClick={onClick}>{isAdmin ? "로그아웃" : "로그인"}</button>
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
@@ -46,18 +56,41 @@ const LoginModal: React.FC = () => {
       >
         {isLoading && <SpinnerBar background={true} />}
         <h1 className={s.h1}>로그인하기</h1>
-        <h2 className={s.h2}>관리자님 환영합니다!</h2>
-        <input
-          className={s.password}
-          type="password"
-          placeholder="비밀번호를 입력하세요."
-          onChange={({ target: { value } }) => setPassword(value)}
-          onKeyDown={checkEnter}
-          autoFocus={true}
-        />
-        <button className={s.loginBtn} onClick={loginClick}>
-          로그인
-        </button>
+        <h2 className={s.h2}>{isAdminLogin ? adminText : userText}</h2>
+        {isAdminLogin ? (
+          <>
+            <input
+              className={s.password}
+              type="password"
+              placeholder="비밀번호를 입력하세요."
+              onChange={({ target: { value } }) => setPassword(value)}
+              onKeyDown={checkEnter}
+              autoFocus={true}
+            />
+            <button className={s.loginBtn} onClick={loginClick}>
+              로그인
+            </button>
+            <hr />
+            <button className={s.changeText} onClick={onChangeState}>
+              또는 사용자 로그인
+            </button>
+          </>
+        ) : (
+          <>
+            <GoogleLogin
+              clientId="957329737930-eb08rfsefqr3es8q8kd9j5ncg9s3r9vh.apps.googleusercontent.com"
+              buttonText="SIGN IN WITH GOOGLE"
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
+              className={s.googleLoginBtn}
+            />
+            <hr />
+            <button className={s.changeText} onClick={onChangeState}>
+              또는 관리자 로그인
+            </button>
+          </>
+        )}
       </Modal>
     </>
   );
