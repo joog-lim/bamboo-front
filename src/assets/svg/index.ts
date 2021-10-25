84e44ee4887a1012fa8b21c7d8e2efc029bd9fe6
iseolin76 <iseolin76@gmail.com>
2021-10-25 09:15:19 +0900

    Add: ThumbsDown, ThumbsUp 이모지 추가

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,2 +1,4 @@
 export { default as Logo } from "./Logo";
 export { default as Spinner } from "./Spinner";
+export { default as ThumbsUp } from "./ThumbsUp";
+export { default as ThumbsDown } from "./ThumbsDown";
