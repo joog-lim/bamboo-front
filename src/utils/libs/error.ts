63c0426edefbe142ad59850f1a1b43fdfd17ff9a
iseolin76 <iseolin76@gmail.com>
2021-10-31 19:04:12 +0900

    Update: switch case에서 에러 핸들러 Object로 조회 방식으로 변경

--- src/utils/libs/error.ts
+++ src/utils/libs/error.ts
@@ -1,19 +1,20 @@
-const errHandler = (err: Error) => {
-  switch (err.message) {
-    case "Request failed with status code 401":
-      alert(
-        "로그인이 필요한 서비스입니다.\n만약 로그인이 되어있다면 다시 로그인해주세요."
-      );
-      break;
-    case "Request failed with status code 400":
-      alert("비밀번호가 잘못되었습니다.");
-      break;
-    case "Request failed with status code 418":
-      alert("이모지 중복 오류\n개발자에게 문의해주세요.");
-      break;
-    default:
-      alert("알 수 없는 에러가 발생하였습니다." + err.message);
-      break;
+interface errMessageObjectType {
+  [key: string]: string;
+}
+
+const errMessageObject: errMessageObjectType = {
+  "Request failed with status code 401":
+    "로그인이 필요한 서비스입니다.\n만약 로그인이 되어있다면 다시 로그인해주세요.",
+  "Request failed with status code 400": "비밀번호가 잘못되었습니다.",
+  "Request failed with status code 418":
+    "이모지 중복 오류\n개발자에게 문의해주세요.",
+};
+
+const errHandler: Function = (err: Error) => {
+  try {
+    alert(errMessageObject[err.message]);
+  } catch (e) {
+    alert("알 수 없는 에러가 발생하였습니다." + err.message);
   }
 };
 
