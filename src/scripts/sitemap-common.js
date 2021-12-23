ecb1743fb949392aad227636e760db67dde00897
iseolin76 <iseolin76@gmail.com>
2021-12-23 10:50:27 +0900

    Create: sitemap script

---
+++ src/scripts/sitemap-common.js
@@ -0,0 +1,60 @@
+// sitemap-common.js
+
+// 패키지 설치
+const fs = require("fs");
+const globby = require("globby");
+const prettier = require("prettier");
+
+// 오늘 날짜 가져오기 & 도메인 설정
+const getDate = new Date().toISOString();
+const CODEIT_DOMAIN = "https://joog-lim.info";
+
+//
+const formatted = (sitemap) => prettier.format(sitemap, { parser: "html" });
+
+(async () => {
+  // 포함할 페이지와 제외할 페이지 등록
+  const pages = await globby([
+    // include
+    "../pages/**/*.tsx",
+    "../pages/*.tsx",
+    "!../pages/_app.tsx",
+    "!../pages/_document.tsx",
+  ]);
+
+  // 파일 경로를 도메인 형태로 변경
+  // ../pages/category/index.tsx -> https://wwww.codeit.kr/category
+  // ../pages/community/threads -> https://wwww.codeit.kr/community/threads
+  const pagesSitemap = `
+    ${pages
+      .map((page) => {
+        const path = page
+          .replace("../pages/", "")
+          .replace(".tsx", "")
+          .replace(/\/index/g, "");
+        const routePath = path === "index" ? "" : path;
+        return `
+          <url>
+            <loc>${CODEIT_DOMAIN}/${routePath}</loc>
+            <lastmod>${getDate}</lastmod>
+          </url>
+        `;
+      })
+      .join("")}`;
+
+  const generatedSitemap = `
+    <urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+      ${pagesSitemap}
+    </urlset>`;
+
+  const formattedSitemap = [formatted(generatedSitemap)];
+
+  fs.writeFileSync(
+    "../../public/sitemap/sitemap-common.xml",
+    formattedSitemap.toString(),
+    "utf8"
+  );
+})();
