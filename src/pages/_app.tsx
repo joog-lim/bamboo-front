a05ab98c3ae504a3102f521b4f32a3b86a743b1f
iseolin76 <iseolin76@gmail.com>
2022-03-17 16:12:42 +0900

    Fix: Worng head tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,25 @@ import "styles/global.scss";
 import { RecoilRoot } from "recoil";
 import ReactModal from "react-modal";
 import { defaultStyles } from "components/modal/modal";
+import Head from "next/head";
 
 ReactModal.setAppElement("#__next");
 ReactModal.defaultStyles = defaultStyles;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <>
+      <Head>
+        <title>광주소프트웨어마이스터고등학교 대나무숲</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </>
   );
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import NextHead from "next/head";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <NextHead>
+        <Head>
           <html lang="ko" />
           <link rel="icon" href="/favicon.ico" />
           <meta charSet="UTF-8" />
-          <title>광주소프트웨어마이스터고등학교 대나무숲</title>
           <meta
             name="description"
             content="대나무 숲은 평소에 하지 못하는 이야기들을 나눌 수 있도록 하는 웹/앱 서비스 입니다. 모두에게 하고 싶던 말, 무엇인가요?"
@@ -19,15 +17,12 @@ export default class MyDocument extends Document {
             content="광주소프트웨어마이스터고등학교, GSM, 대나무숲, 광대숲, 죽림, Joog-lim, 광주소프트웨어마이스터고등학교 대나무숲"
           />
           <meta name="author" content="jooglim.org@gmail.com" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
           {/* 오픈그래프 */}
           <meta property="og:url" content="https://joog-lim.info" />
           <meta
             property="og:title"
             content="광주소프트웨어마이스터고등학교 대나무숲"
+            key="title"
           />
           <meta
             property="og:description"
@@ -46,8 +41,7 @@ export default class MyDocument extends Document {
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:site" content="@joog-lim" />
           <meta name="twitter:creator" content="@https://joog-lim.info" />
-        </NextHead>
-        <Head />
+        </Head>
         <body>
           <div id="root">
             <Main />
